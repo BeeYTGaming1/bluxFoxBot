58473e07cd61db7537c1c13ff9c71e5eef9cdfe9
Rem-Chan <91677261+kimihiko123@users.noreply.github.com>
2022-01-08 05:28:09 +0700

    Update message.js

--- events/message.js
+++ events/message.js
@@ -4,16 +4,16 @@ client.on("message", async (message) => {
     if (
         message.author.bot ||
         !message.guild ||
-        !message.content.toLowerCase().startsWith(client.config.prefix)
+ !message.content.toLowerCase().startsWith(client.config.prefix)
     )
         return;
-
+if(!message.channel.nsfw) return message.channel.send("Bạn vô kênh nsfw mà coi nhé!")
     const [cmd, ...args] = message.content
         .slice(client.config.prefix.length)
         .trim()
         .split(/ +/g);
 
-    const command = client.commands.get(cmd.toLowerCase()) || client.commands.find(c => c.aliases?.includes(cmd.toLowerCase()));
+    const command = client.commands.get(cmd.toLowerCase())
 
     if (!command) return;
     await command.run(client, message, args);
